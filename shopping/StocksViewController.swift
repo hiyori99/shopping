583794f8935ed9006d8594f48386e238895ad13b
hiyori99 <luluikdkoko@gmail.com>
2021-09-02 15:16:30 +0900

    delete secondviewcontroller.swift

---
+++ shopping/StocksViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StocksViewController.swift
+//  shopping
+//
+//  Created by 池田光里 on 2021/09/02.
+//
+
+import UIKit
+
+class StocksViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
